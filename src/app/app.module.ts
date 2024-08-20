a93da1c121f88da3501d966b64d36b8901ae7dc1
Vito Bozzano <vi.bozzano@duocuc.cl>
2024-08-20 17:16:34 -0400

    unos html de contacto carpeta nueva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
