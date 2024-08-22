a37d56f3ed7a6cee1606724af9e8ecdce78fa4ca
Vito Bozzano <vi.bozzano@duocuc.cl>
2024-08-22 17:30:40 -0400

    agregado de servicios y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactoComponent
+    ContactoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
